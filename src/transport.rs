251950f1d8449bf42cfccf27e97c94c07546ef6c
Aleks Rutins <aleks@rutins.com>
2022-06-23 21:36:40 -0400

    wip

--- src/transport.rs
+++ src/transport.rs
@@ -1,4 +1,6 @@
-use druid::{Data, Lens};
+use std::time::Instant;
+
+use druid::{Data, Lens, Widget, Size, TimerToken, Event};
 
 #[derive(Clone, Data)]
 pub struct Transport {
@@ -55,5 +57,39 @@ impl Transport {
 
     pub fn current_time(&self) -> u64 {
         self.current_time
+    }
+}
+
+
+/// A widget that manages a timer to manage the transport. Phew! That's a lot of management.
+struct TransportManager {
+    timer_id: TimerToken,
+    last_update: Instant
+}
+
+impl Widget<Transport> for TransportManager {
+    fn event(&mut self, ctx: &mut druid::EventCtx, event: &druid::Event, data: &mut Transport, env: &druid::Env) {
+        match event {
+            Event::WindowConnected => {
+
+            },
+            _ => ()
+        }
+    }
+
+    fn lifecycle(&mut self, ctx: &mut druid::LifeCycleCtx, event: &druid::LifeCycle, data: &Transport, env: &druid::Env) {
+        
+    }
+
+    fn update(&mut self, ctx: &mut druid::UpdateCtx, old_data: &Transport, data: &Transport, env: &druid::Env) {
+        
+    }
+
+    fn layout(&mut self, ctx: &mut druid::LayoutCtx, bc: &druid::BoxConstraints, data: &Transport, env: &druid::Env) -> druid::Size {
+        bc.constrain(Size::new(0., 0.))
+    }
+
+    fn paint(&mut self, ctx: &mut druid::PaintCtx, data: &Transport, env: &druid::Env) {
+        
     }
 }
